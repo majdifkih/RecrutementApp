320248cadbc6790075c888cab3698581c479b98f
majdi <majdi@majdi.com>
2024-01-07 12:45:36 +0100

    commit

--- migrations/Version20231210170904.php
+++ migrations/Version20231210170904.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231205175411 extends AbstractMigration
+final class Version20231210170904 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20231205175411 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE offre ADD req_skills LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE candidat DROP skills');
+        $this->addSql('ALTER TABLE offre ADD location VARCHAR(255) DEFAULT NULL, ADD time_type VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE offre DROP req_skills');
+        $this->addSql('ALTER TABLE candidat ADD skills LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE offre DROP location, DROP time_type');
     }
 }
